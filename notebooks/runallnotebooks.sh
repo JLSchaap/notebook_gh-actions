55daa3f394a24d148aafceb88ca1d38e3e31c6a4
John Schaap <JLSchaap@users.noreply.github.com>
2023-02-14 16:20:25 +0100

    Update runallnotebooks.sh

--- notebooks/runallnotebooks.sh
+++ notebooks/runallnotebooks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# script to run all notebooks 
+# script to run all notebooks modified
 jupyter nbconvert --to script *.ipynb
 find .  -name '*.py' -exec sh -c 'python "$1" > "$1.out"' _ {} \;
