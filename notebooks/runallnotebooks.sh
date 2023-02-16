ecdeefde297a836e8be1918491e89a30486661be
John Schaap <JLSchaap@users.noreply.github.com>
2023-02-16 09:32:37 +0100

    linefeed modfied

--- notebooks/runallnotebooks.sh
+++ notebooks/runallnotebooks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# script to run all notebooks modified
+# script to run all notebooks 
 jupyter nbconvert --to script *.ipynb
 find .  -name '*.py' -exec sh -c 'python "$1" > "$1.out"' _ {} \;
